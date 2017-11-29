b6e70e54a75e742ff383d5e8347d23406ea0d312
FlaviusMT <hoangtrinh1001@gmail.com>
2017-11-29 10:15:59 +0700

    complete_load_data

--- support_function.py
+++ support_function.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def get_feature(img_name):
     """
@@ -18,3 +19,32 @@ def get_feature(img_name):
     #print(img_feature_data)
 
     return img_feature_name, img_feature_data
+
+def get_features_labels_from_folder(name_folder_direct, datatype):
+    db_ROOT = name_folder_direct
+    name_file_direct = db_ROOT + "/" + str(datatype) +".txt"
+    Y = []
+    X = []
+
+    with open(name_file_direct) as name_file:
+        names = name_file.read().splitlines()
+
+    for name in names:
+        X.append(get_feature(name)[1])
+
+
+    X = np.asarray(X)
+    X = X.reshape((X.shape[0], X.shape[2]))
+
+    #print(X.shape)
+
+    label_file_direct = db_ROOT + "/" + "lb" + str(datatype) + ".txt"
+
+    with open(label_file_direct) as label_file:
+        Y = label_file.read().splitlines()
+
+    Y = np.array(Y)
+    Y = Y.reshape((1, Y.shape[0])).T
+    #print(Y.shape)
+    return X, Y
+
--- train_dev_split.py
+++ train_dev_split.py
@@ -1,37 +1,7 @@
-import numpy as np
-import os
-from sklearn.model_selection import  train_test_split
 import glob
+import os
 
-
-
-
-# will adjust later
-# def load_all_feature_label():
-#     ROOT = 'features/'
-#     iROOT = 'images/'
-#     f = os.listdir('features')
-#     X = []
-#     Y = []
-#
-#     for folder in f:
-#         direct = ROOT + folder + '/'
-#         i_direct = iROOT + folder + '/'
-#         i_direct = i_direct + folder + ".txt"
-#
-#         feature_files = os.listdir(direct)
-#         for feature in feature_files:
-#             temp = np.load(direct + feature)
-#             X.append(temp)
-#             with open(i_direct) as label_file:
-#                 Y.append(int(label_file.readline().replace('\n','')))
-#
-#     X = np.array(X)
-#     X = X.reshape((X.shape[0], X.shape[2]))
-#     Y = np.array(Y)
-#     Y = Y.reshape((1, Y.shape[0])).T
-#     return X,Y
-
+from sklearn.model_selection import train_test_split
 
 
 def one_random_split(dev_size):
